23e111681a3dacf1b54959e4420e681eac8fd8fe
moneyhurricane <37752748+moneyhurricane@users.noreply.github.com>
2019-04-03 22:53:25 -0400

    Seperating Image and Sprite Variables

--- code.js
+++ code.js
@@ -7,6 +7,29 @@ var playerLocY = 0;
 var barbaraVelocity = 1;
 var deltaX = 0;
 var deltaY = 0;
+//Image and Animation Variables
+var miniMountainsImg;
+var mapSuburbiaImg;
+var floor1Img;
+var facebookLogoImg;
+var stopKarenImg;
+var startButtonGreenImg;
+var startButtonRedImg;
+var welcomeTextImg;
+var gameDescImg;
+var gameInstructImg;
+var wasdInstructAnim;
+var arrowInstructAnim;
+var minivanImg;
+var liveLaughLoveImg;
+var PBJImg;
+var houseImg;
+var snowPlatformImg;
+var snowPlatform2Img;
+var barbaraImg;
+var characterMotionsImg;
+var healthBarImg;
+var healthBar2Img;
 //Sprite Variables
 var miniMountains;
 var mapSuburbia;
@@ -34,33 +57,31 @@ var healthBar;
 var healthBar2;
 
 function preload() {
-  miniMountains = loadImage('https://i.ibb.co/Zd5gtYd/Minivan-Mountains.jpg');
-  mapSuburbia = loadImage('https://i.ibb.co/GM8g3yM/Map-Suburbia.png');
-  floor1 = loadImage('https://i.ibb.co/mX7Twx2/Floor.png');
-  facebookLogo = loadImage('https://i.ibb.co/HxrQfLw/Facebook-Logo.png');
-  stopKaren = loadImage('https://i.ibb.co/pbZHhPB/Stop-Karen-Logo.png');
-  startButton = loadImage('https://i.ibb.co/m8ytpsC/Start-Button-Green.png');
-  startButton2 = loadImage('https://i.ibb.co/m8ytpsC/Start-Button-Green.png');
-  startButton3 = loadImage('https://i.ibb.co/m8ytpsC/Start-Button-Green.png');
-  startButton4 = loadImage('https://i.ibb.co/tQmn00Q/Start-Button-Red.png');
-  welcomeText = loadImage('https://i.ibb.co/PZM58Zd/Welcome-Text.png');
-  gameDesc = loadImage('https://i.ibb.co/xqnFh14/Game-Description.png');
-  gameInstruct = loadImage('https://i.ibb.co/bRstth1/Game-Instructions.png');
-  wasdInstruct = loadAnimation('https://i.ibb.co/3Tc4pfm/wasdUp.png', 'https://i.ibb.co/XjT1b6z/wasdLeft.png', 'https://i.ibb.co/RPJWgpT/wasdDown.png', 'https://i.ibb.co/BBhfppJ/wasd-Right.png');
-  arrowInstruct = loadAnimation('https://i.ibb.co/QcH2s37/arrowUp.png', 'https://i.ibb.co/PCTBPrj/arrow-Left.png', 'https://i.ibb.co/0JYWSpH/arrow-Down.png', 'https://i.ibb.co/TgKm85F/arrow-Right.png');
-  minivan = loadImage('https://i.ibb.co/M2mbK8r/Minivan-Icon.png');
-  liveLaughLove = loadImage('https://i.ibb.co/SBhXsSD/Live-Laugh-Love-Icon.png');
-  PBJ = loadImage('https://i.ibb.co/zGQM2gw/PB-amp-JIcon.png');
-  house = loadImage('https://i.ibb.co/gmgDnqB/House.png');
-  snowPlatform = loadImage('https://i.ibb.co/Zm60dBC/Snow-Platform.png');
-  snowPlatform2 = loadImage('https://i.ibb.co/Zm60dBC/Snow-Platform.png');
-  barbara = loadImage('https://i.ibb.co/tbBrf9B/barbara.png');
-  characterStand = loadImage('https://i.ibb.co/c2jrkbQ/character-Stand-Right.png');
-  characterRunLeft = loadAnimation('https://i.ibb.co/fY2qjr1/Character-Run-Frame1.png', 'https://i.ibb.co/g9YBRRH/Character-Run-Frame2.png');
-  characterRunRight = loadAnimation('https://i.ibb.co/Xt4yxsr/Character-Left-Run-Frame1.png', 'https://i.ibb.co/0VwJ4Y3/Character-Left-Run-Frame2.png');
-  characterJumpRight = loadImage('https://i.ibb.co/QcBD5rs/Character-Jump.png');
-  characterJumpLeft = loadImage('https://i.ibb.co/6mDW624/Character-Left-Jump.png');
-  fullHealthBar = loadImage('https://i.ibb.co/RjCGB91/Full-Health-Bar.png');
+  miniMountainsImg = loadImage('https://i.ibb.co/Zd5gtYd/Minivan-Mountains.jpg');
+  mapSuburbiaImg = loadImage('https://i.ibb.co/GM8g3yM/Map-Suburbia.png');
+  floor1Img = loadImage('https://i.ibb.co/mX7Twx2/Floor.png');
+  facebookLogoImg = loadImage('https://i.ibb.co/HxrQfLw/Facebook-Logo.png');
+  stopKarenImg = loadImage('https://i.ibb.co/pbZHhPB/Stop-Karen-Logo.png');
+  startButtonGreenImg = loadImage('https://i.ibb.co/m8ytpsC/Start-Button-Green.png');
+  startButtonRedImg = loadImage('https://i.ibb.co/tQmn00Q/Start-Button-Red.png');
+  welcomeTextImg = loadImage('https://i.ibb.co/PZM58Zd/Welcome-Text.png');
+  gameDescImg = loadImage('https://i.ibb.co/xqnFh14/Game-Description.png');
+  gameInstructImg = loadImage('https://i.ibb.co/bRstth1/Game-Instructions.png');
+  wasdInstructImg = loadAnimation('https://i.ibb.co/3Tc4pfm/wasdUp.png', 'https://i.ibb.co/XjT1b6z/wasdLeft.png', 'https://i.ibb.co/RPJWgpT/wasdDown.png', 'https://i.ibb.co/BBhfppJ/wasd-Right.png');
+  arrowInstructImg = loadAnimation('https://i.ibb.co/QcH2s37/arrowUp.png', 'https://i.ibb.co/PCTBPrj/arrow-Left.png', 'https://i.ibb.co/0JYWSpH/arrow-Down.png', 'https://i.ibb.co/TgKm85F/arrow-Right.png');
+  minivanImg = loadImage('https://i.ibb.co/M2mbK8r/Minivan-Icon.png');
+  liveLaughLoveImg = loadImage('https://i.ibb.co/SBhXsSD/Live-Laugh-Love-Icon.png');
+  PBJImg = loadImage('https://i.ibb.co/zGQM2gw/PB-amp-JIcon.png');
+  houseImg = loadImage('https://i.ibb.co/gmgDnqB/House.png');
+  snowPlatformImg = loadImage('https://i.ibb.co/Zm60dBC/Snow-Platform.png');
+  snowPlatform2Img = loadImage('https://i.ibb.co/Zm60dBC/Snow-Platform.png');
+  barbaraImg = loadImage('https://i.ibb.co/tbBrf9B/barbara.png');
+  characterStandImg = loadImage('https://i.ibb.co/c2jrkbQ/character-Stand-Right.png');
+  characterRunLeftImg = loadAnimation('https://i.ibb.co/fY2qjr1/Character-Run-Frame1.png', 'https://i.ibb.co/g9YBRRH/Character-Run-Frame2.png');
+  characterRunRightImg = loadAnimation('https://i.ibb.co/Xt4yxsr/Character-Left-Run-Frame1.png', 'https://i.ibb.co/0VwJ4Y3/Character-Left-Run-Frame2.png');
+  characterJumpRightImg = loadImage('https://i.ibb.co/QcBD5rs/Character-Jump.png');
+  characterJumpLeftImg = loadImage('https://i.ibb.co/6mDW624/Character-Left-Jump.png');
+  fullHealthBarImg = loadImage('https://i.ibb.co/RjCGB91/Full-Health-Bar.png');
 }
 
 function setup() {
@@ -68,123 +89,123 @@ function setup() {
 
 //minivan mountains
 miniMountains = createSprite(200,200);
-miniMountains.addImage(miniMountains);
+miniMountains.addImage(miniMountainsImg);
 miniMountains.visible = false;
 //map
 mapSuburbia = createSprite(200,200);
-mapSuburbia.addImage(mapSuburbia);
+mapSuburbia.addImage(mapSuburbiaImg);
 mapSuburbia.scale = 0.67;
 mapSuburbia.visible = false;
 //floor
 floor1 = createSprite(200,400,400);
-floor1.addImage(floor1);
+floor1.addImage(floor1Img);
 //facebook logo
 facebookLogo = createSprite(75,100);
-facebookLogo.addImage(facebookLogo);
+facebookLogo.addImage(facebookLogoImg);
 facebookLogo.scale = 0.13;
 facebookLogo.setVelocity(0,3);
 //title
 stopKaren = createSprite(260,100);
-stopKaren.addImage(stopKaren);
+stopKaren.addImage(stopKarenImg);
 stopKaren.height = 400;
 stopKaren.width = 270;
 stopKaren.setVelocity(0,3);
 //start button
 startButton = createSprite(200,300);
-startButton.addImage(startButton);
+startButton.addImage(startButtonGreenImg);
 startButton.scale = 0.5;
 startButton.setCollider("circle");
 //start button 2
 startButton2 = createSprite(200,50);
-startButton2.addImage(startButton);
+startButton2.addImage(startButtonGreenImg);
 startButton2.scale = 0.5;
 startButton2.setCollider("circle");
 startButton2.visible = false;
 //start button 3
 startButton3 = createSprite(200,200);
-startButton3.addImage(startButton);
+startButton3.addImage(startButtonGreenImg);
 startButton3.scale = 0.5;
 startButton3.setCollider("circle");
 startButton3.visible = false;
 //start button 4
 startButton4 = createSprite(200,375);
-startButton4.addImage(startButton2);
+startButton4.addImage(startButtonRedImg);
 startButton4.scale = 0.1;
 startButton4.visible = false;
 //welcome text
 welcomeText = createSprite(200,300);
-welcomeText.addImage(welcomeText);
+welcomeText.addImage(welcomeTextImg);
 welcomeText.scale = 0.5;
 welcomeText.visible = false;
 //game description
 gameDesc = createSprite(200,150);
-gameDesc.addImage(gameDesc);
+gameDesc.addImage(gameDescImg);
 gameDesc.scale = 0.5;
 gameDesc.visible = false;
 //game instructions
 gameInstruct = createSprite(200,300);
-gameInstruct.addImage(gameInstruct);
+gameInstruct.addImage(gameInstructImg);
 gameInstruct.scale = 0.5;
 gameInstruct.visible = false;
 //wasd instructions
 wasdInstruct = createSprite(100,100);
-wasdInstruct.addAnimation(wasdInstruct);
+wasdInstruct.addAnimation(wasdInstructAnim);
 wasdInstruct.scale = 0.15;
 wasdInstruct.visible = false;
 //arrow instructions
 arrowInstruct = createSprite(300,100);
-arrowInstruct.addAnimation(arrowInstruct);
+arrowInstruct.addAnimation(arrowInstructAnim);
 arrowInstruct.scale = 0.8;
 arrowInstruct.visible = false;
 //minivan
 minivan = createSprite(84,93);
-minivan.addImage(minivan);
+minivan.addImage(minivanImg);
 minivan.scale = 0.4;
 minivan.visible = false;
 //live laugh love
 liveLaughLove = createSprite(324,246);
-liveLaughLove.addImage(liveLaughLove);
+liveLaughLove.addImage(liveLaughLoveImg);
 liveLaughLove.scale = 0.23;
 liveLaughLove.visible = false;
 //peanut butter jelly
 PBJ = createSprite(240,58);
-PBJ.addImage(PBJ);
+PBJ.addImage(PBJImg);
 PBJ.scale = 0.14;
 PBJ.visible = false;
 //house(s)
 house = createSprite(371,721);
-house.addImage(house);
+house.addImage(houseImg);
 house.scale = 0.09;
 house.visible = false;
 //snow platforms
 snowPlatform = createSprite(300,200);
-snowPlatform.addImage(snowPlatform);
+snowPlatform.addImage(snowPlatformImg);
 snowPlatform.scale = 0.2;
 snowPlatform.setVelocity(0,-1);
 snowPlatform.visible = false;
 snowPlatform2 = createSprite(100,75);
-snowPlatform2.addImage(snowPlatform);
+snowPlatform2.addImage(snowPlatformImg);
 snowPlatform2.scale = 0.2;
 snowPlatform2.setVelocity(0,1);
 snowPlatform2.visible = false;
 //barbara
 barbara = createSprite(300,295);
-barbara.addImage(barbara);
+barbara.addImage(barbaraImg);
 barbara.scale = 0.15;
 barbara.visible = false;
 //character motions
 characterMotions = createSprite(50,300);
-characterMotions.addImage(characterStand);
+characterMotions.addImage(characterStandImg);
 characterMotions.scale = 0.5;
 characterMotions.visible = false;
 //health bar
 healthBar = createSprite(50,300);
 healthBar.scale = 0.25;
-healthBar.addImage(fullHealthBar);
+healthBar.addImage(fullHealthBarImg);
 healthBar.visible = false;
 healthBar2 = createSprite(50,300);
 healthBar2.scale = 0.25;
-healthBar2.addImage(fullHealthBar);
+healthBar2.addImage(fullHealthBarImg);
 healthBar2.visible = false;
 }
 
